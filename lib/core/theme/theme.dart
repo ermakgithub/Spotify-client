9e91061a8e4746b409c02681b9baaf9953dec885
unknown <deniseermak@gmail.com>
2025-09-26 01:14:22 +0300

    Signup text and Custom Field

---
+++ lib/core/theme/theme.dart
@@ -0,0 +1,9 @@
+import 'package:client/core/theme/app_pallete.dart';
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final darkThemeMode = ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: Pallete.backgroundColor,
+    inputDecorationTheme: InputDecorationTheme(),
+  );
+}
