020d7a113d31a60018f4534f3565e9977a4c04a1
unknown <deniseermak@gmail.com>
2025-09-30 00:55:02 +0300

    SingUp page

--- lib/core/widgets/custom_field.dart
+++ lib/core/widgets/custom_field.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
 
 class CustomField extends StatelessWidget {
-  const CustomField({super.key});
+  final String hintText;
+  final TextEditingController controller;
+  final bool isObscureText;
+  const CustomField({
+    super.key,
+    required this.hintText,
+    required this.controller,
+    this.isObscureText = false,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(decoration: InputDecoration());
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(hintText: hintText),
+      validator: (val) {
+        if (val!.trim().isEmpty) {
+          return "$hintText is missing!";
+        }
+        return null;
+      },
+      obscureText: isObscureText,
+      obscuringCharacter: '*',
+    );
   }
 }
